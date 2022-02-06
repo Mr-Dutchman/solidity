2f256c73662bae1b8832e2658d893c8118072513
Mr-Dutchman <meelisfidelis@gmail.com>
2022-02-06 12:31:04 +0100

    signature contract

---
+++ signature.js
@@ -0,0 +1,16 @@
+// var hash = web3utils.sha3("mesage to be signed");
+// web3utils.eth.personal.sign(hash,web3.eth.defaultAccount, function() {console.log("signed");
+// });
+//
+// recipient is the address that should be paid.
+// amount, in wei, specifies how much ether should be sent.
+// nonce can be any unique number to prevent replay attacks
+// contractAddress is used to prevent cross-contract replay attacks
+
+function signPayment(recipient, amount, nonce, contractAddress, callback) {
+    var hash ="0x" + abi.soliditySHA3(
+        ["address", "uint256", "uint256", "address"],
+        [recipient, amount,nonce, contractAddress]
+    ).toString("hex");
+    web3.eth.personal.sign(hash, web3.eth.defaultAddress, callback)
+}
